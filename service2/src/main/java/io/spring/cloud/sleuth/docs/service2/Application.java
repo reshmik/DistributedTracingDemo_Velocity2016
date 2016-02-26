33231613288d46ab811965e08abd9546341ea920
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-02-26 13:51:10 +0100

    Added span reporter

--- service2/src/main/java/io/spring/cloud/sleuth/docs/service2/Application.java
+++ service2/src/main/java/io/spring/cloud/sleuth/docs/service2/Application.java
@@ -6,12 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @RestController
 public class Application {
 
---
+++ service3/src/main/java/io/spring/cloud/sleuth/docs/service3/StoringZipkinSpanReporter.java
@@ -0,0 +1,57 @@
+package io.spring.cloud.sleuth.docs.service3;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.metric.SpanReporterService;
+import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
+import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
+import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import zipkin.Span;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@Component
+@RestController
+public class StoringZipkinSpanReporter implements ZipkinSpanReporter {
+
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	private final Multimap<Long, Span> sentSpans = Multimaps.synchronizedListMultimap(
+			LinkedListMultimap.create());
+	private final HttpZipkinSpanReporter delegate;
+
+	@Autowired
+	public StoringZipkinSpanReporter(SpanReporterService spanReporterService, ZipkinProperties zipkin) {
+		delegate = new HttpZipkinSpanReporter(zipkin.getBaseUrl(), zipkin.getFlushInterval(),
+				spanReporterService);
+	}
+
+	@RequestMapping("/spans/{traceId}")
+	public ResponseEntity<Collection<Span>> spans(@PathVariable String traceId) {
+		Collection<Span> spansForTrace = sentSpans
+				.get(org.springframework.cloud.sleuth.Span.hexToId(traceId));
+		return ResponseEntity.ok(spansForTrace);
+	}
+
+	@Override
+	public void report(Span span) {
+		sentSpans.put(span.traceId, span);
+		log.info("Sending span [\n\n" + span.toString() + "\n\n] to Zipkin");
+		delegate.report(span);
+	}
+}
