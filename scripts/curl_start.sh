a74ab2d2ad9235e25cce00bf6f1ddf00bec91e22
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-06-14 14:56:56 +0200

    Fixing scripts

--- scripts/curl_start.sh
+++ scripts/curl_start.sh
@@ -4,4 +4,4 @@ set -e
 
 echo -e "Sending a request to service1"
 
-curl --fail localhost:8081/start && echo -e "\nIt worked!" || echo -e "\nFailed to send the request" && exit 1
+curl --fail localhost:8081/start && echo -e "\nIt worked!" && exit 0 || echo -e "\nFailed to send the request" && exit 1
