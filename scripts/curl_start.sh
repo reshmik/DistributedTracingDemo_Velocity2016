3fb27fce831a591761b1373489f5b4d251467da7
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-06-14 14:48:50 +0200

    Added more logging

--- scripts/curl_start.sh
+++ scripts/curl_start.sh
@@ -4,4 +4,4 @@ set -e
 
 echo -e "Sending a request to service1"
 
-curl --fail localhost:8081/start
+curl --fail localhost:8081/start && echo -e "\nIt worked!" || echo -e "\nFailed to send the request" && exit 1
